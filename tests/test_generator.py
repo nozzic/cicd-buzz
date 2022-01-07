f2ce1949570fb4424373ac502ded7e8257d08e34
root <root@Slient-Orange.localdomain>
2022-01-07 14:06:31 +0100

    lint

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,11 +3,13 @@ import unittest
 from buzz import generator
 
 def test_sample_single_word():
+    
     l = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
     assert word in l
 
 def test_sample_multiple_words():
+
     l = ('foo', 'bar', 'foobar')
     words = generator.sample(l, 2)
     assert len(words) == 2
@@ -16,5 +18,6 @@ def test_sample_multiple_words():
     assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():
+
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
